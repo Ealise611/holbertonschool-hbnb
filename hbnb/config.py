40f3b8bbdb1506686f82916a1c1e2bed49d24bf4
Sammy <myallio94@gmail.com>
2025-07-09 13:08:14 +1000

    Added: a testing config stage for postman testing

--- hbnb/config.py
+++ hbnb/config.py
@@ -8,8 +8,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/hbnb_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/hbnb_test_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'test-jwt-secret-key-for-pytest'
 
 config = {
     'development': DevelopmentConfig,
+    'testing': TestingConfig,
     'default': DevelopmentConfig
 }

6c967016738ae5086c56ce10d6c7dd4a29b866fe
Ealise <ealise.wang@gmail.com>
2025-08-13 20:18:25 +1000

    fix port

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -20,7 +20,10 @@ def create_app(config_class=None):
         origins=[
             "http://127.0.0.1:3001",
             "http://localhost:3001",
-            "http://127.0.0.1:5500",
+            "http://127.0.0.1:5000",
+            "http://127.0.0.1:3000",
+            "http://localhost:3000",
+            "http://localhost:5000",
             "file://",
         ],
     )
